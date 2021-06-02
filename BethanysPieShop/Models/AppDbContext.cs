6eec923cc75cadf39c4944845ce60b3708c646b4
TroyNilo <alllose2troy@gmail.com>
2021-06-02 13:00:45 +0800

    Creating Order form

--- BethanysPieShop/Models/AppDbContext.cs
+++ BethanysPieShop/Models/AppDbContext.cs
@@ -15,6 +15,8 @@ namespace BethanysPieShop.Models
         public DbSet<Pie> Pies { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
+        public DbSet<Order> Order { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -28,7 +28,10 @@ namespace BethanysPieShop
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
             services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+           
             services.AddHttpContextAccessor();
             services.AddSession();
             services.AddControllersWithViews();
